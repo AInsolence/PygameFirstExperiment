2fdfe9f6f06ea6d5772c9f3b96d9a16acaee3345
AInsolence <antoniovbak14@gmail.com>
2016-09-16 19:53:14 +0600

    debagged collide func, tested objects, added starships images

--- main.py
+++ main.py
@@ -5,39 +5,42 @@ from pygame import *
 #main variables
 win_width = 850
 win_hight = 535
-main_window = (win_width, win_hight)
+main_window = ((win_width - 10), (win_hight - 10))
 #create window
 window = pygame.display.set_mode(main_window)
-pygame.display.set_caption('Shoot!!!')
+pygame.display.set_caption('My First Game!!!')
 #create gameplay screen
 background_image = pygame.image.load('image/firstStage_background.jpg')
-screen = pygame.Surface((int(win_width), int(win_hight)))
+screen = pygame.Surface((win_width, win_hight))
 
 '''Check collisions of objects in our game'''
 def collide(object1, object2):
-    if (object1.xpos > object2.xpos - object1.width) and (object1.xpos < object2.xpos + object1.width) and (object1.ypos > object2.ypos - object1.hight) and (object1.ypos < object2.ypos + object1.hight):
+    if ((object1.xpos > object2.xpos - object1.xwidth) and (object1.xpos < object2.xpos + object2.xwidth) and (object1.ypos > object2.ypos - object1.yheight) and (object1.ypos < object2.ypos + object1.yheight)):
         return True
     return False
 
 '''Create object with classes'''
 class Sprite:
-    def __init__(self, xpos = 0, ypos = 0, filename):
+    def __init__(self, xpos, ypos, filename):
         self.xpos = xpos
         self.ypos = ypos
         self.bitmap = pygame.image.load(filename)# get object image, where filename = path to image
         #self.bitmap.set_colorkey((0, 0, 0))# make sprites background color invisible if it exists
-        xwidth = 50
-        yheight = 50
+    xwidth = 60
+    yheight = 50
     def render(self):
         screen.blit(self.bitmap, (self.xpos, self.ypos))
 '''Game objects'''
-hero = Sprite((win_width/2), (win_hight + yheight), 'image/monster5050.png')
-aim = Sprite(0, 100, 'image/squeaky_mouse.png')
+hero = Sprite((win_width/2), (win_hight - 100), 'image/starship_1.PNG')
+aim = Sprite(0, 100, 'image/enemy_ship_1.png')
+aim.xwidth = 100
+aim.yheight = 50
 aim.go_right = True
 aim.go_down = False
-bullet = Sprite(-10, 0, 'image/bullet.png')
-bullet.xwidth = 20
-bullet.yheight = 20
+aim.speed = 1
+bullet = Sprite(win_hight - 60, (hero.xpos - 25), 'image/bullet.png')
+bullet.xwidth = 10
+bullet.yheight = 10
 bullet.push = False
 
 '''Key settings'''
@@ -45,44 +48,49 @@ pygame.key.set_repeat(1,1)
 
 '''Main Game Loop'''
 play = True
+scores = 0
 while play:
     for e in pygame.event.get():   # !!!EVENT Handler block!!!
         if e.type == pygame.QUIT:
+            print ('Your record is ', scores, ' scores!!!', '\n', 'Congratulations!')
+            print ('Your best level is ', int(level + 1))
             play = False
         '''Movements'''
         if e.type == pygame.KEYDOWN:
             '''Hero movements'''
             #x axis movement
             if e.key == pygame.K_LEFT:
-                if hero.xpos != 0:
-                    hero.xpos -= 1
+                if hero.xpos > 0:
+                    hero.xpos -= 3
             if e.key == pygame.K_RIGHT:
-                if hero.xpos != win_width - hero.xwidth:
-                    hero.xpos += 1  
+                if hero.xpos < win_width - hero.xwidth - 10:
+                    hero.xpos += 3  
             #y axis movement
             if e.key == pygame.K_UP:
-                if hero.ypos != 150:
-                    hero.ypos -= 1
+                if hero.ypos >= win_hight - 200:
+                    hero.ypos -= 3
             if e.key == pygame.K_DOWN:
-                if hero.ypos != win_hight - hero.yheight:
-                    hero.ypos += 1 
+                if hero.ypos <= win_hight - hero.yheight - 40:
+                    hero.ypos += 3 
             '''Keybord buttons push events'''
             # bullet push
             if e.key == pygame.K_SPACE:
                 if bullet.push == False:
-                    bullet.xpos = hero.xpos + (hero.width/2)
+                    bullet.xpos = hero.xpos + (hero.xwidth/2)
+                    bullet.ypos = hero.ypos + (hero.yheight/2)
                     bullet.push = True
-        '''Collizion handler'''
-        if collide(aim, bullet):
-            bullet.push = False
+    '''Collizion handler'''
+    if collide(aim, bullet):
+        bullet.push = False
+        scores += 10    
             
     '''Aim movement block'''                
     if aim.go_right:
-        aim.xpos += 1
-        if aim.xpos > 800:
+        aim.xpos += aim.speed
+        if aim.xpos > 730:
             aim.go_right = False
     else:
-        aim.xpos -= 1
+        aim.xpos -= aim.speed
         if aim.xpos < 0:
             aim.go_right = True
     
@@ -90,16 +98,20 @@ while play:
     if bullet.ypos < 0:
         bullet.push = False
     if bullet.push == False:
-        bullet.ypos = -10
+        bullet.ypos = win_hight
     else:
-        bullet.ypos -= 1
+        bullet.ypos -= 5
     
+    '''Difficalty level'''
+    level = int(scores/30)
+    aim.speed = level + 1
+
     
     '''Object reflection block'''
     pygame.display.flip()# update all on the screen
-    pygame.time.delay(3)# delay time of all game process, argument in milliseconds
+    pygame.time.delay(1)# delay time of all game process, argument in milliseconds
     window.blit(screen, (0,0))# display screen
     screen.blit(background_image, (0,0))# show background image
+    bullet.render()
     hero.render()
     aim.render()
-    bullet.render()
